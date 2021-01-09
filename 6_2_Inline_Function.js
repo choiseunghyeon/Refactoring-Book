ac5e42d4fed989d9a79c6b3d417585030102bd9e
choiseunghyeon <ket8780@gmail.com>
2021-01-09 17:09:13 +0900

    6.2 inline function practice

--- 6_2_Inline_Function.js
+++ 6_2_Inline_Function.js
@@ -1,18 +1,11 @@
 function rating(aDriver) {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 function reportLines(aCustomer) {
   const lines = [];
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
   gatherCustomerData(lines, aCustomer);
   return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
-  out.push(["name", aCustomer.name]);
-  out.push(["location", aCustomer.location]);
 }
