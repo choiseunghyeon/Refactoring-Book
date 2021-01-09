df3a0e354eb8bbf57995975a28fc65216cb631f2
choiseunghyeon <ket8780@gmail.com>
2021-01-09 17:30:41 +0900

    6.3 extract variable pratice

--- 6_3_Extract_Variable.js
+++ 6_3_Extract_Variable.js
@@ -1,6 +1,8 @@
 function price(order) {
-  // 가격(price) = 기본 가격 - 수량 할인 + 배송비
-  return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
 }
 
 class Order {
@@ -16,6 +18,18 @@ class Order {
   }
 
   get price() {
-    return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+
+  get basePrice() {
+    return order.quantity * order.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(basePrice * 0.1, 100);
   }
 }
