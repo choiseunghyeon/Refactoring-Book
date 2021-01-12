011a9a254787e63704f61b91cb640539618cf437
choiseunghyeon <ket8780@gmail.com>
2021-01-12 21:45:57 +0900

    added 7.1 / 7.2 e.g

--- 7_1_Encapsulate Record.js
+++ 7_1_Encapsulate Record.js
@@ -1,11 +1,9 @@
-const organization = new Organization({ name: "최승현", country: "KR"});
+// 수정 전
+const organization = { name: "최승현", country: "KR"};
+let  result = `<h1>${organization.name}</h1>`; // 읽기 예
+organization.name = "William"; // 쓰기 예
 
-let  result = `<h1>${getOrganization().name}</h1>`; // 읽기 예
-getOrganization().name = "William"; // 쓰기 예
-
-function getOrganization() {return organization;}
-
-
+// 수정 후 
 // 레코드 캡슐화의 이점은 데이터 조작 방식을 통제할 수 있기 때문
 class Organization { 
     constructor(data) {
@@ -18,3 +16,9 @@ class Organization {
     get country() {return this._data.country}
     set country(aCountryCode) {this._data.country = aCountryCode}
 }
+
+const organization = new Organization({ name: "최승현", country: "KR"});
+function getOrganization() {return organization;}
+
+let  result = `<h1>${getOrganization().name}</h1>`; // 읽기 예
+getOrganization().name = "William"; // 쓰기 예
--- 7_2_Encapsulate_Collection.js
+++ 7_2_Encapsulate_Collection.js
@@ -1,3 +1,43 @@
+// 수정 전
+class Person {
+    constructor(name) {
+        this._name = name;
+        this._courses = [];
+    }
+
+    get name() {return this._courses._name;}
+    get courses() {return this._courses;}
+    set courses(aList) {return this._courses = aList;}
+    
+}
+
+class Courses {
+    constructor(name, isAdvanced) {
+        this._name = name;
+        this._isAdvanced = isAdvanced;
+    }
+
+    get name() {return this._name;}
+    get isAdvanced() {return this._isAdvanced;}
+}
+
+
+// 사용 예
+const numAdvancedCourses = aPerson.courses.filter(c => c.isAdvanced).length;
+
+function client1 () {
+    const basicCourseNames = readBasicCourseNames(filename);
+    aPerson.courses = basicCourseNames.map(new Courses(name, false));
+}
+
+function client2 () {
+    for (const name of readBasicCourseNames(filename)) {
+        aPerson.courses.push(new Courses(name, false));
+    }
+}
+
+
+// 수정 후
 class Person {
     constructor(name) {
         this._name = name;
