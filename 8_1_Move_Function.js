70a42b02ab01c4d90c6520869dba9b29b263b0d6
choiseunghyeon <ket8780@gmail.com>
2021-01-17 01:39:28 +0900

    8.1 Move Function practice

--- 8_1_Move_Function.js
+++ 8_1_Move_Function.js
@@ -1,41 +1,40 @@
 // e.g 1 중첩 함수를 최상위로 옮기기
 function trackSummary(points) {
   const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
-  const pace = totalTime / 60 / totalDistance;
+  const pace = totalTime / 60 / totalDistance(points);
   return {
     time: totalTime,
-    distance: totalDistance,
+    distance: totalDistance(points),
     pace,
   };
-
-  function calculateDistance() {
-    // 총 거리 계산
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
-
-  function distance(p1, p2) {
-    // 두 지점의 거리 계산
-    const EARTH_RADIUS = 3959; // 단위 : 마일(mile)
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(radians(p2.lat)) * Math.cos(radians(p1.lat)) * Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-  }
-
-  function radians(degrees) {
-    // 라디안 값으로 변환
-    return (degrees * Math.PI) / 180;
-  }
 
   function calculateTime() {
     // 총 시간 계산
   }
+}
+
+function totalDistance(points) {
+  // 총 거리 계산
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+}
+
+function distance(p1, p2) {
+  // 두 지점의 거리 계산
+  const EARTH_RADIUS = 3959; // 단위 : 마일(mile)
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(radians(p2.lat)) * Math.cos(radians(p1.lat)) * Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return EARTH_RADIUS * c;
+}
+
+function radians(degrees) {
+  // 라디안 값으로 변환
+  return (degrees * Math.PI) / 180;
 }
 
 // e.g 2 다른 클래스로 옮기기
@@ -49,11 +48,17 @@ class Account {
   }
 
   get overdraftCharge() {
+    return this.type.overdraftCharge(this.daysOverdrawn);
+  }
+}
+
+class AccountType {
+  overdraftCharge(daysOverdrawn) {
     //초과 인출 이자 계산
-    if (this.type.isPremium) {
+    if (this.isPremium) {
       const baseCharge = 10;
-      if (this.daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-    } else return this.daysOverdrawn * 1.75;
+      if (daysOverdrawn <= 7) return baseCharge;
+      else return baseCharge + (daysOverdrawn - 7) * 0.85;
+    } else return daysOverdrawn * 1.75;
   }
 }
