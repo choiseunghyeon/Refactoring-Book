5ea3eacf6aca856ce3d35ddc6d956da90cd29896
choiseunghyeon <ket8780@gmail.com>
2021-01-17 16:23:12 +0900

    8.3 Move Statements into Function practice

--- 8_3_Move_Statements into Function.js
+++ 8_3_Move_Statements into Function.js
@@ -2,18 +2,12 @@ function renderPerson(outStream, person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>제목: ${person.photo.title}</p>`); // 제목 출력
-  result.push(emitPhotoData(person.photo));
+  result.push(emitPhotoData(person));
   return result.join("\n");
 }
 
 function photoDiv(p) {
-  return [
-    "<div>",
-    `<p>제목: ${p.title}</p>`, // 제목 출력
-    emitPhotoData(p),
-    "</div>",
-  ].join("\n");
+  return ["<div>", emitPhotoData(p), "</div>"].join("\n");
 }
 
 function emitPhotoData(aPhoto) {
@@ -21,4 +15,12 @@ function emitPhotoData(aPhoto) {
   result.push(`<p>위치: ${aPhoto.location}</p>`);
   result.push(`<p>위치: ${aPhoto.date.toDateString()}</p>`);
   return result.join("\n");
+}
+
+function emitPhotoData(p) {
+  return [
+    `<p>제목: ${p.title}</p>`, // 제목 출력
+    `<p>위치: ${aPhoto.location}</p>`,
+    `<p>위치: ${aPhoto.date.toDateString()}</p>`,
+  ].join("\n");
 }
