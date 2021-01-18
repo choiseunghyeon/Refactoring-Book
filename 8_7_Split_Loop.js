c7fe37dc8977158905bf67a3e0652620df84c35d
choiseunghyeon <ket8780@gmail.com>
2021-01-18 23:16:35 +0900

    8.7 Split Loop practice

--- 8_7_Split_Loop.js
+++ 8_7_Split_Loop.js
@@ -1,9 +1,32 @@
 function splitLoop() {
-  let youngest = people[0] ? people[0].age : Infinity;
+  return `최연소 ${youngestAge()}, 총 급여 ${totalSalary()}`;
+}
+
+function totalSalary() {
   let totalSalary = 0;
   for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
     totalSalary += p.salary;
   }
-  return `최연소 ${youngest}, 총 급여 ${totalSalary}`;
+  return totalSalary;
+}
+
+function youngestAge() {
+  let youngest = people[0] ? people[0].age : Infinity;
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age;
+  }
+  return youngest;
+}
+
+// split loop is for next step like Replace loop with pipe line / Substitute Algorithm
+function splitLoop() {
+  return `최연소 ${youngestAge()}, 총 급여 ${totalSalary()}`;
+}
+
+function totalSalary() {
+  return people.reduce((total, p) => total + p.salary);
+}
+
+function youngestAge() {
+  return Math.min(...people.map(p => p.age));
 }
