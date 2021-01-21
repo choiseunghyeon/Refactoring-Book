dbe524fa7ec33236d2f564fd19d3859fb86f79b6
choiseunghyeon <ket8780@gmail.com>
2021-01-21 22:10:21 +0900

    10.1 Decompose Conditional practice

--- 10_1_Decompose_Conditional.js
+++ 10_1_Decompose_Conditional.js
@@ -1,7 +1,19 @@
 // 여름철이면 할인율이 달라지는 어떤 서비스의 요금을 계산
 let charge;
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
-  charge = quantity * plan.summerRate;
+if (summer()) {
+  charge = summerCharge();
 } else {
-  charge = quantity * plan.regularRate + plan.regularServiceCharge;
+  charge = regularCharge();
+}
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+
+function summerCharge() {
+  return quantity * plan.summerRate;
+}
+
+function regularCharge() {
+  return quantity * plan.regularRate + plan.regularServiceCharge;
 }
