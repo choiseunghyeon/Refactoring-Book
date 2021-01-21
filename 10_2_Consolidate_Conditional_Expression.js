220d6e0faaf2a088b0196dbbb1c236367a5bfc2a
choiseunghyeon <ket8780@gmail.com>
2021-01-21 22:15:49 +0900

    10.2 Consolidate Conditional Expression practice

--- 10_2_Consolidate_Conditional_Expression.js
+++ 10_2_Consolidate_Conditional_Expression.js
@@ -1,6 +1,21 @@
 function disabilityAmount(anEmployee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
-  if (anEmployee.isPartTime) return 0;
+  if (isNotEligibleForDisability()) return 0;
   // 장애 수당 계산
+
+  // e.g AND Before
+  //   if (anEmployee.onVacation) {
+  //     if (anEmployee.seniority > 10) {
+  //       return 1;
+  //     }
+  //   }
+  //   return 0.5;
+
+  // e.g AND After
+  if (anEmployee.onVacation && anEmployee.seniority > 10) return 1;
+  return 0.5;
+
+  function isNotEligibleForDisability() {
+    //장애 수당 적용 여부 확인
+    return anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime;
+  }
 }
