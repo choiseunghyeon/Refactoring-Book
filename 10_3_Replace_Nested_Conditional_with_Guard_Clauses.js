83cfbc15a0f2a72f46ce6210274942b559d3b139
choiseunghyeon <ket8780@gmail.com>
2021-01-23 01:15:23 +0900

    10.3 Replace Nested Conditional With Guard Clauses practice

--- 10_3_Replace_Nested_Conditional_with_Guard_Clauses.js
+++ 10_3_Replace_Nested_Conditional_with_Guard_Clauses.js
@@ -2,32 +2,18 @@
 
 // e.g 1
 function payAmount(employee) {
-  let result;
-  if (employee.isPeparated) {
-    //퇴사한 직원인가?
-    result = { amount: 0, reasonCode: "SEP" };
-  } else {
-    if (employee.isRetired) {
-      // 은퇴한 직원인가?
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 급여 계산 로직
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      result = someFinalComputation();
-    }
-  }
-  return result;
+  if (employee.isPeparated) return { amount: 0, reasonCode: "SEP" };
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+
+  // 급여 계산 로직
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  return someFinalComputation();
 }
 
 // e.g 2 조건 반대로 만들기
 function adjustedCapital(anInstrument) {
-  let result = 0;
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-    }
-  }
-  return result;
+  if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
+  return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
