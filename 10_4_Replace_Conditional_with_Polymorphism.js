97af6f1f17bbb0ad11b32ba5b98733aaccf39292
choiseunghyeon <ket8780@gmail.com>
2021-01-24 17:35:37 +0900

    10.4 Replace Conditional With Polymorphism practice

--- 10_4_Replace_Conditional_with_Polymorphism.js
+++ 10_4_Replace_Conditional_with_Polymorphism.js
@@ -1,86 +1,178 @@
 // e.g 1
 function plumages(birds) {
-  return new Map(birds.map(b => [b.name, plumage(b)]));
+  return new Map(birds.map(b => createBird(b)).map(bird => [bird.name, bird.plumage]));
 }
 
 function speeds(birds) {
-  return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+  return new Map(birds.map(b => createBird(b)).map(bird => [bird.name, bird.airSpeedVelocity]));
 }
 
-function plumage(bird) {
-  // 깃털 상태, 로직은 알 필요 없음
+function createBird(bird) {
   switch (bird.type) {
     case "유럽 제비":
-      return "보통이다";
+      return new EuropeanSwallow(bird);
     case "아프리카 제비":
-      return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+      return new AfricanSwallow(bird);
     case "노르웨이 파랑 앵무":
-      return bird.voltage > 100 ? "그을렸다" : "예쁘다";
+      return new NorwegianBlueParrot(bird);
     default:
-      return "알 수 없다.";
+      return new Bird(bird);
+  }
+}
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    return "알 수 없다.";
+  }
+
+  get airSpeedVelocity() {
+    return null;
   }
 }
 
-function airSpeedVelocity(bird) {
-  // 비행 속도, 로직은 알 필요 없음
-  switch (bird.type) {
-    case "유럽 제비":
-      return 35;
-    case "아프리카 제비":
-      return 40 - 2 * bird.numberOfCoconuts;
-    case "노르웨이 파랑 앵무":
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
+class EuropeanSwallow extends Bird {
+  constructor(bird) {
+    super(bird);
+  }
+
+  get plumage() {
+    return "보통이다";
+  }
+
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  constructor(bird) {
+    super(bird);
+  }
+
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  constructor(bird) {
+    super(bird);
+  }
+
+  get plumage() {
+    return this.voltage > 100 ? "그을렸다" : "예쁘다";
+  }
+
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
   }
 }
 
 // e.g 2
 function rating(voyage, history) {
-  // 투자 등급
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > vr + chr * 2) return "A";
-  else return "B";
+  return createRating(voyage, history).value;
 }
 
-function voyageRisk(voyage) {
-  // 항해 경로 위험 요소
-  let result = 1;
-  if (voyage.length > 4) result += 2;
-  if (voyage.length) result += voyage.length - 8;
-  if (["중국", "동인도"].includes(voyage.zone)) result += 4;
-  return Math.max(result, 0);
+function createRating(voyage, history) {
+  if (voyage.zone === "중국" && history.some(v => "중국" === v.zone)) return new ExperiencedChinaRating(voyage.history);
+  else return new Rating(voyage.history);
 }
 
-function captainHistoryRisk(voyage, history) {
-  // 선장의 항해 이력 위험요소
-  let result = 1;
-  if (history.length < 5) result += 4;
-  result += history.filter(v => v.profit < 0).length;
-  if (voyage.zone === "중국" && hasChina(history)) result -= 2;
-  return Math.max(result, 0);
-}
-
-function hasChina(history) {
-  //중국을 경유하는가?
-  return history.some(v => "중국" === v.zone);
-}
-
-function voyageProfitFactor(voyage, history) {
-  // 수익 요인
-  let result = 2;
-  if (voyage.zone === "중국") result += 1;
-  if (voyage.zone === "동인도") result += 1;
-  if (voyage.zone === "중국" && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (voyage.length > 12) result += 1;
-    if (voyage.length > 18) result -= 1;
-  } else {
-    if (history.length > 8) result += 1;
-    if (voyage.length > 14) result -= 1;
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
   }
-  return result;
+
+  get value() {
+    // 투자 등급
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
+    if (vpf * 3 > vr + chr * 2) return "A";
+    else return "B";
+  }
+  get voyageRisk() {
+    // 항해 경로 위험 요소
+    let result = 1;
+    if (this.voyage.length > 4) result += 2;
+    if (this.voyage.length) result += this.voyage.length - 8;
+    if (["중국", "동인도"].includes(this.voyage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
+
+  get captainHistoryRisk() {
+    // 선장의 항해 이력 위험요소
+    let result = 1;
+    if (this.history.length < 5) result += 4;
+    result += this.history.filter(v => v.profit < 0).length;
+    return Math.max(result, 0);
+  }
+
+  get hasChinaHistory() {
+    //중국을 경유하는가?
+    return this.history.some(v => "중국" === v.zone);
+  }
+
+  get voyageProfitFactor() {
+    // 수익 요인
+    let result = 2;
+    if (this.voyage.zone === "중국") result += 1;
+    if (this.voyage.zone === "동인도") result += 1;
+    result += this.historyLengthFactor;
+    result += this.voyageLengthFactor;
+    return result;
+  }
+
+  get voyageLengthFactor() {
+    return this.voyage.length > 14 ? -1 : 0;
+  }
+
+  get historyLengthFactor() {
+    return this.history.length > 8 ? 1 : 0;
+  }
 }
+
+class ExperiencedChinaRating extends Rating {
+  constructor(voyage, history) {
+    super(voyage, history);
+  }
+
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
+
+  get voyageProfitFactor() {
+    return super.voyageProfitFactor + 3;
+  }
+
+  get voyageLengthFactor() {
+    let result = 0;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
+  }
+
+  get historyLengthFactor() {
+    return this.history.length > 10 ? 1 : 0;
+  }
+}
+
+// 호출 코드
+const voyage = { zone: "서인도", length: 10 };
+const history = [
+  { zone: "동인도", profit: 5 },
+  { zone: "서인도", profit: 15 },
+  { zone: "중국", profit: -2 },
+  { zone: "서아프리카", profit: 7 },
+];
+
+const myRating = rating(voyage, history);
