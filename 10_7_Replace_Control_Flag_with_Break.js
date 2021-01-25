1937812864614b4a871b46b7a0a8cd6492e6cc83
choiseunghyeon <ket8780@gmail.com>
2021-01-25 21:16:43 +0900

    10.7 Replace Control Flag With Break practice

--- 10_7_Replace_Control_Flag_with_Break.js
+++ 10_7_Replace_Control_Flag_with_Break.js
@@ -1,19 +1,24 @@
 
 function someFunc() {
     // 생략 (중요하지 않은 코드)
-    let found = false;
-    for (const p of people) {
-        if (! found) {
-            if (p === "조커") {
-                sendAlert();
-                found = true;
-            }
-            if (p === "사루만") {
-                sendAlert();
-                found = true;
-            }
-        }
-    }
+    checkForMiscreants(people);
 
     // 생략
+}
+
+function checkForMiscreants(people) {
+    for (const p of people) {
+        if (p === "조커") {
+            sendAlert();
+            return;
+        }
+        if (p === "사루만") {
+            sendAlert();
+            return;
+        }
+    }
+}
+
+function checkForMiscreants(people) {
+    if (people.some(p => ["조커", "사루만"].includes(p))) sendAlert();
 }
