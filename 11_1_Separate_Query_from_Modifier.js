716bd9b1513416d3ec6f37bf824b62b6b88af624
choiseunghyeon <ket8780@gmail.com>
2021-01-26 21:49:57 +0900

    11.1 Separate Query From Modifier e.g

---
+++ 11_1_Separate_Query_from_Modifier.js
@@ -0,0 +1,17 @@
+function alertForMiscreant(people) {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+      return "조커";
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+      return "사루만";
+    }
+  }
+  return "";
+}
+
+function client() {
+  const found = alertForMiscreant(people);
+}
