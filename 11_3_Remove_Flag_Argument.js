705735a69682ecd42d6b41b2ce64161888db05e4
choiseunghyeon <ket8780@gmail.com>
2021-01-26 22:20:09 +0900

    11.3 Remove Flag Argumnet e.g

---
+++ 11_3_Remove_Flag_Argument.js
@@ -0,0 +1,41 @@
+// e.g 1
+function deliveryDate(anOrder, isRush) {
+  if (isRush) {
+    let deliveryTime;
+    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+  } else {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+  }
+}
+
+function client1() {
+  ashipment.adliveryDate = deliveryDate(anOrder, true);
+}
+
+function client2() {
+  ashipment.adliveryDate = deliveryDate(anOrder, false);
+}
+
+// e.g 2 매개변수를 까다로운 방식으로 사용할 때
+function deliveryDate(anOrder, isRush) {
+  let result;
+  let deliveryTime;
+  if (anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT") deliveryTime = isRush ? 1 : 2;
+  else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH") {
+    deliveryTime = 2;
+    if (anOrder.deliveryState === "NH" && !isRush) deliveryTime = 3;
+  } else if (isRush) deliveryTime = 3;
+  else if (anOrder.deliveryState === "ME") deliveryTime = 3;
+  else deliveryTime = 4;
+
+  result = anOrder.placedOn.plusDays(2 + deliveryTime);
+  if (isRush) result = result.minusDays(1);
+  return result;
+}
