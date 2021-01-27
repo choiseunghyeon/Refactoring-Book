6dd54b31c7ef82bf2acb90713eb1754dcb555980
choiseunghyeon <ket8780@gmail.com>
2021-01-27 18:54:48 +0900

    11.4 Preserve Whole Object practice

--- 11_4_Preserve_Whole_Object.js
+++ 11_4_Preserve_Whole_Object.js
@@ -1,13 +1,13 @@
 function client() {
-    const low = aRoom.daysTempRange.low;
-    const high = aRoom.daysTempRange.high;
-    if (!aPlan.withinRange(low, high)) {
+    const tempRange = aRoom.daysTempRange
+    if (!aPlan.withinRange(tempRange)) {
         alerts.push("방 온도가 지정 범위를 벗어났습니다.")
     }
 }
 
 class HeatingPlan {
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
+    withinRange(tempRange) {
+        return (tempRange.low >= this._temperatureRange.low) && (tempRange.high <= this._temperatureRange.high);
     }
 }
+
