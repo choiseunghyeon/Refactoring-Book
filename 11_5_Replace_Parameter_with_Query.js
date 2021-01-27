93cf4b57b9cb904b3985cc5aea25165540fcca54
choiseunghyeon <ket8780@gmail.com>
2021-01-27 18:59:05 +0900

    11.5 Replace Parameter With Query  practice

--- 11_5_Replace_Parameter_with_Query.js
+++ 11_5_Replace_Parameter_with_Query.js
@@ -1,16 +1,17 @@
 class Order {
     get finalPrice() {
         const basePrice = this.quantity * this.itemPrice;
-        let discountLevel;
-        if (this.quantity > 100) discountLevel = 2;
-        else discountLevel = 1;
-        return this.dicountedPrice(basePrice, discountLevel);
+        return this.dicountedPrice(basePrice);
     }
 
-    discountedPrice(basePrice, discountLevel) {
-        switch(discountLevel) {
+    discountedPrice(basePrice) {
+        switch(this.discountLevel) {
             case 1: return basePrice * 0.95;
             case 2: return basePrice * 0.9;
         }
+    }
+
+    get discountLevel() {
+        return this.quantity > 100 ? 2 : 1;
     }
 }
