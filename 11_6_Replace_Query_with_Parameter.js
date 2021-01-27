26abd4245f7a21a4cca34e7fcaa10a10ebd40777
choiseunghyeon <ket8780@gmail.com>
2021-01-27 19:26:09 +0900

    11.6 Replace Query With Parameter practice

--- 11_6_Replace_Query_with_Parameter.js
+++ 11_6_Replace_Query_with_Parameter.js
@@ -1,15 +1,15 @@
 // 참조 투명성 유지를 위해 (순수함수) - 같은 값을 건네면 매번 똑같은 결과를 내는 함수는 다루기 쉽다.
 
 class HeatingPlan {
-    get targetTemperature() {
-        if (thermostate.selectedTemperature > this._max) return this._max;
-        else if (thermostate.selectedTemperature < this._min) return this._min;
-        else return thermostate.selectedTemperature;
+    get targetTemperature(selectedTemperature) {
+        if (selectedTemperature > this._max) return this._max;
+        else if (selectedTemperature < this._min) return this._min;
+        else return selectedTemperature;
     }
 }
 
 function client() {
-    if (thePlan.targetTemperature > thermostate.currentTemperature) setToHeat();
-    if (thePlan.targetTemperature < thermostate.currentTemperature) setToCool();
+    if (thePlan.targetTemperature(thermostate.selectedTemperature) > thermostate.currentTemperature) setToHeat();
+    if (thePlan.targetTemperature(thermostate.selectedTemperature) < thermostate.currentTemperature) setToCool();
     else setOff();
 }
