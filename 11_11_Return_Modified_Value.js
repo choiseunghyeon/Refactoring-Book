e188e98e2c7d25d4d71be16408fa3ca5a02c66be
choiseunghyeon <ket8780@gmail.com>
2021-01-29 22:03:03 +0900

    11.11 Return Modified Value practice

--- 11_11_Return_Modified_Value.js
+++ 11_11_Return_Modified_Value.js
@@ -1,14 +1,13 @@
-let totalAscent = 0;
-let totalTime = 0;
-let totalDisatance = 0;
-calculateAscent();
-calculateTime();
-calculateDisatance();
+let totalAscent = calculateAscent();
+let totalTime = calculateTime();
+let totalDisatance = calculateDisatance();
 const pace = totalTime / 60 / totalDisatance;
 
 function calculateAscent() {
+  let result = 0;
   for (let i = 1; i < points.length; i++) {
     const verticalChange = points[i].elevation - points[i - 1].elevation;
-    totalAscent += verticalChange > 0 ? verticalChange : 0;
+    result += verticalChange > 0 ? verticalChange : 0;
   }
+  return result;
 }
