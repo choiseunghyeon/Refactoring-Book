a6e51522559b2a8a4c0f864e74cb874b22b9b09e
choiseunghyeon <ket8780@gmail.com>
2021-01-30 19:17:54 +0900

    12.8 Extract Superclass practice

--- 12_8_Extract_Superclass.js
+++ 12_8_Extract_Superclass.js
@@ -1,49 +1,48 @@
-class Employee {
-  constructor(name, id, monthlyCost) {
-    this._id = id;
+class Party {
+  constructor(name) {
     this._name = name;
+  }
+
+  get name() {
+    // 이름
+    return this._name;
+  }
+
+  get annualCost() {
+    // 연간 비용
+    return this.monthlyCost * 12;
+  }
+}
+
+class Employee extends Party {
+  constructor(name, id, monthlyCost) {
+    super(name);
+    this._id = id;
     this._monthlyCost = monthlyCost;
   }
   get monthlyCost() {
     // 월간 비용
     return this._monthlyCost;
   }
-  get name() {
-    // 이름
-    return this._name;
-  }
   get id() {
     return this._id;
   }
-
-  get annualCost() {
-    // 연간 비용
-    return this._monthlyCost * 12;
-  }
 }
 
-class Department {
+class Department extends Party {
   constructor(name, staff) {
-    this._name = name;
+    super(name);
     this._staff = staff;
   }
-  get totalMonthlyCost() {
-    // chd 월간 비용
+  get monthlyCost() {
+    // 월간 비용
     return this.staff.map(e => e.monthlyCost).reduce((sum, cost) => sum + cost);
   }
   get staff() {
     return this._staff.slice();
   }
-  get name() {
-    // 이름
-    return this._name;
-  }
 
   get headCount() {
     return this.staff.length;
-  }
-  get totalAnnualCost() {
-    // 총 연간 비용
-    return this._monthlyCost * 12;
   }
 }
