904f158e01774a087ec31507faeec9440863c762
choiseunghyeon <ket8780@gmail.com>
2021-01-30 15:28:30 +0900

    12.1 Pull Up Method practice

--- 12_1_Pull_up_Method.js
+++ 12_1_Pull_up_Method.js
@@ -1,13 +1,12 @@
-class Party {}
-
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
-}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
+  get monthlyCost() {
+    throw new SubclassResponsibilityError();
   }
 }
+
+class Employee extends Party {}
+
+class Department extends Party {}
