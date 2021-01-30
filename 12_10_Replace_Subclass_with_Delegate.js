6794fc13e055a07f541fa4fa33249930e3c0b1ea
choiseunghyeon <ket8780@gmail.com>
2021-01-31 00:13:18 +0900

    12.10 Replace Subclass With Delegate practice

--- 12_10_Replace_Subclass_with_Delegate.js
+++ 12_10_Replace_Subclass_with_Delegate.js
@@ -1,4 +1,13 @@
 // e.g 1 서브 클래스가 하나일 떄
+function createBooking(show, date) {
+  return new Booking(show, date);
+}
+
+function createPremiumBooking(show, date, extras) {
+  const result = new Booking(show, date, extras);
+  result._bePremium(extras);
+  return result;
+}
 
 class Booking {
   constructor(show, date) {
@@ -7,60 +16,73 @@ class Booking {
   }
 
   get hasTalkBack() {
-    return this._show.hasOwnProperty("talkback") && !this.isPeakDay;
+    return this._premiumDelegate ? this._premiumDelegate.hasTalkBack : this._show.hasOwnProperty("talkback") && !this.isPeakDay;
   }
 
   get basePrice() {
     let result = this._show.price;
     if (this.isPeakDay) result += Math.round(result * 0.15);
-    return result;
+    return this._premiumDelegate ? this._premiumDelegate.extendBasePrice(result) : result;
+  }
+
+  get hasDinner() {
+    return this._premiumDelegate ? this._premiumDelegate.hasDinner : undefined;
+  }
+
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
   }
 }
 
-class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
     this._extras = extras;
   }
 
   get hasTalkBack() {
-    return this._show.hasOwnProperty("talkback");
-  }
-
-  get basePrice() {
-    return Math.round(super.basePrice + this._extras.premiumFee);
+    return this._host._show.hasOwnProperty("talkback");
   }
 
   get hasDinner() {
-    return this._extras.hasOwnProperty("dinner") && !this.isPeakDay;
+    return this._extras.hasOwnProperty("dinner") && !this._host.isPeakDay;
+  }
+
+  extendBasePrice(base) {
+    return Math.round(base + this._extras.premiumFee);
   }
 }
 
 function client() {
-  aBooking = new Booking(show, date);
+  aBooking = createBooking(show, date);
 }
 function client2() {
-  aBooking = new PremiumBooking(show, date, extras);
+  aBooking = createPremiumBooking(show, date, extras);
 }
 
 // e.g 2 서브 클래스가 여러개일 때
 
 function createBird(bird) {
-  switch (bird.type) {
-    case "유럽 제비":
-      return new EuropeanSwallow(bird);
-    case "아프리카 제비":
-      return new AfricanSwallow(bird);
-    case "노르웨이 파랑 앵무":
-      return new NorwegianBlueParrot(bird);
-    default:
-      return new Bird(bird);
-  }
+  return new Bird(bird);
 }
 class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
+    this._speciesDelegate = this.selectSpeciedDelegate(data);
+  }
+
+  selectSpeciedDelegate(data) {
+    switch (data.type) {
+      case "유럽 제비":
+        return new EuropeanSwallowDelegate(data, this);
+      case "아프리카 제비":
+        return new AfricanSwallowDelegate(data, this);
+      case "노르웨이 파랑 앵무":
+        return new NorwegianBlueParrot(data, this);
+      default:
+        return new SpeciesDelegate(data, this);
+    }
   }
 
   get name() {
@@ -68,7 +90,21 @@ class Bird {
   }
 
   get plumage() {
-    return this._plumage || "보통이다";
+    return this._speciesDelegate.plumage;
+  }
+
+  get airSpeedVelocity() {
+    return this._speciesDelegate.airSpeedVelocity;
+  }
+}
+
+class SpeciesDelegate {
+  constructor(data, bird) {
+    this._bird = bird;
+  }
+
+  get plumage() {
+    return this._bird._plumage || "보통이다";
   }
 
   get airSpeedVelocity() {
@@ -76,15 +112,15 @@ class Bird {
   }
 }
 
-class EuropeanSwallow extends Bird {
+class EuropeanSwallowDelegate extends SpeciesDelegate {
   get airSpeedVelocity() {
     return 35;
   }
 }
 
-class AfricanSwallow extends Bird {
-  constructor(data) {
-    super(data);
+class AfricanSwallowDelegate extends SpeciesDelegate {
+  constructor(data, bird) {
+    super(data, bird);
     this._numberOfCoconuts = data.numberOfCoconuts;
   }
 
@@ -93,16 +129,16 @@ class AfricanSwallow extends Bird {
   }
 }
 
-class NorwegianBlueParrot extends Bird {
-  constructor(bird) {
-    super(bird);
+class NorwegianBlueParrotDelegate extends SpeciesDelegate {
+  constructor(data, bird) {
+    super(data, bird);
     this._voltage = data.voltage;
     this._isNailed = data.isNailed;
   }
 
   get plumage() {
     if (this.voltage > 100) return "그을렸다";
-    else return this._plumage || "예쁘다";
+    else return this._bird._plumage || "예쁘다";
   }
 
   get airSpeedVelocity() {
