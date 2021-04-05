4019f489e7566ccd5338721dba6abe41fac47958
choiseunghyeon <ket8780@gmail.com>
2021-04-05 22:19:26 +0900

    01 예시. replace temp with query

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -13,7 +13,7 @@ function statement(invoice, plays) {
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = 0;
 
     thisAmount = amountFor(perf, play);
@@ -30,22 +30,26 @@ function statement(invoice, plays) {
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
   let result = 0;
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
