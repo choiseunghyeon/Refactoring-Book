1757fd06a5c102c3d46e4ab500af1d92ed964646
choiseunghyeon <ket8780@gmail.com>
2021-04-06 21:00:34 +0900

    01 예시. 중간 데이터 구조로 옮기기

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -7,12 +7,15 @@ import invoice from "./invoices.js";
 import plays from "./plays.js";
 console.log(statement(invoice[0], plays));
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, invoice, plays);
 
-  function renderPlainText(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer}\n)`;
+  function renderPlainText(data, invoice, plays) {
+    let result = `청구 내역 (고객명: ${data.customer}\n)`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       // 청구 내역을 출력한다.
       result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석\n)`;
     }
