81ac68a0c36d92808f19ffd45f793e50189635f9
choiseunghyeon <ket8780@gmail.com>
2021-04-06 20:36:02 +0900

    01 예시. 반복문 쪼개기 및 문장 슬라이드

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -8,14 +8,17 @@ import plays from "./plays.js";
 console.log(statement(invoice[0], plays));
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer}\n)`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석\n)`;
     totalAmount += amountFor(perf);
+  }
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `총액 ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
