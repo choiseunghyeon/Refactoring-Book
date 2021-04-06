7de9d8d0d6c58e02c22aed5ce8c0185567925549
choiseunghyeon <ket8780@gmail.com>
2021-04-06 21:25:24 +0900

    01 예시. 리팩터링 끝

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -5,78 +5,25 @@
  */
 import invoice from "./invoices.js";
 import plays from "./plays.js";
+import createStatementData from "./createStatementData.js";
 console.log(statement(invoice[0], plays));
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumneCredits = totalVolumneCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
+}
 
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer}\n)`;
+
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석\n)`;
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
+  result += `총액 ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumneCredits}점\n`;
+  return result;
 
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, perf) => (total += perf.amount), 0);
-  }
-
-  function totalVolumneCredits(data) {
-    return data.performances.reduce((total, perf) => (total += perf.volumeCredits), 0);
-  }
-
-  function renderPlainText(data, plays) {
-    let result = `청구 내역 (고객명: ${data.customer}\n)`;
-
-    for (let perf of data.performances) {
-      // 청구 내역을 출력한다.
-      result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석\n)`;
-    }
-
-    result += `총액 ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumneCredits}점\n`;
-    return result;
-
-    function usd(aNumber) {
-      return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
-    }
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
   }
 }
