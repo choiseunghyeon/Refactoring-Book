d1c2ef2bf1fbc0b53b3f10b8de1d6dd05942d0d2
choiseunghyeon <ket8780@gmail.com>
2021-04-06 21:34:37 +0900

    01 예시. html용 렌더링 기능 추가

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -10,6 +10,9 @@ console.log(statement(invoice[0], plays));
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
+}
 
 function renderPlainText(data) {
   let result = `청구 내역 (고객명: ${data.customer}\n)`;
@@ -22,8 +25,22 @@ function renderPlainText(data) {
   result += `총액 ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumneCredits}점\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (const perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>`;
+  result += `<p>적립 포인트: <em>${usd(data.totalVolumneCredits)}</em>점</p>`;
+  return result;
 }
