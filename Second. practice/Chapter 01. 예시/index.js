9963cde10b290599942e421a11677d7253d6089f
choiseunghyeon <ket8780@gmail.com>
2021-04-06 20:56:25 +0900

    01 예시. 단계 쪼개기 및 함수 추출하기

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -7,66 +7,71 @@ import invoice from "./invoices.js";
 import plays from "./plays.js";
 console.log(statement(invoice[0], plays));
 function statement(invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer}\n)`;
+  return renderPlainText(invoice, plays);
 
-  for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석\n)`;
-  }
+  function renderPlainText(invoice, plays) {
+    let result = `청구 내역 (고객명: ${invoice.customer}\n)`;
 
-  result += `총액 ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumneCredits()}점\n`;
-  return result;
-
-  function totalAmount() {
-    let result = 0;
     for (let perf of invoice.performances) {
-      result += amountFor(perf);
+      // 청구 내역을 출력한다.
+      result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석\n)`;
     }
-    return result;
-  }
 
-  function totalVolumneCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+    result += `총액 ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumneCredits()}점\n`;
+    return result;
+
+    function totalAmount() {
+      let result = 0;
+      for (let perf of invoice.performances) {
+        result += amountFor(perf);
+      }
+      return result;
     }
-    return volumeCredits;
-  }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    function totalVolumneCredits() {
+      let result = 0;
+      for (let perf of invoice.performances) {
+        result += volumeCreditsFor(perf);
+      }
+      return result;
     }
-    return result;
+
+    function volumeCreditsFor(aPerformance) {
+      let result = 0;
+      result += Math.max(aPerformance.audience - 30, 0);
+      if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+      return result;
+    }
+
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+      let result = 0;
+      switch (playFor(aPerformance).type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+      }
+      return result;
+    }
+
+    function usd(aNumber) {
+      return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
+    }
   }
-}
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
 }
