72ec6bf3ec920cb6eaf4cc15a8c54ce8cf0b27e6
choiseunghyeon <ket8780@gmail.com>
2021-04-06 20:28:29 +0900

    01 예시. Extract Function

--- Second. practice/Chapter 01. 예시/index.js
+++ Second. practice/Chapter 01. 예시/index.js
@@ -13,11 +13,7 @@ function statement(invoice, plays) {
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    //포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    //희극 관객 5명 마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-
+    volumeCredits += volumeCreditsFor(perf);
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석\n)`;
     totalAmount += amountFor(perf);
@@ -25,6 +21,13 @@ function statement(invoice, plays) {
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
