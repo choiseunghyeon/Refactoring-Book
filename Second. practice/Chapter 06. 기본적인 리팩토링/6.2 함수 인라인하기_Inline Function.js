b2ca5b3f0c35158ea830f4f081e2e55568408de4
choiseunghyeon <ket8780@gmail.com>
2021-04-20 22:38:09 +0900

    6.2 함수 인라인하기_Inline Function before refactoring

---
+++ Second. practice/Chapter 06. 기본적인 리팩토링/6.2 함수 인라인하기_Inline Function.js
@@ -0,0 +1,21 @@
+// 1
+function rating(aDriver) {
+  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(dvr) {
+  return dvr.numberOfLateDeliveries > 5;
+}
+
+// 2
+
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
+}
