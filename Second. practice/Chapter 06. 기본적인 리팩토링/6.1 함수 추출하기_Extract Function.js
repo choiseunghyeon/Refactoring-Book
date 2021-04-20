91cd20076d64edcdadbdfa9e010c025c23f78dd1
choiseunghyeon <ket8780@gmail.com>
2021-04-20 22:22:34 +0900

    6.1 함수 추출하기_Extract Function before refactoring

--- Second. practice/Chapter 06. 기본적인 리팩토링/6.1 함수 추출하기_Extract Function.js
+++ Second. practice/Chapter 06. 기본적인 리팩토링/6.1 함수 추출하기_Extract Function.js
@@ -59,3 +59,35 @@ function printBanner() {
   console.log("*** 고객 채무 ***");
   console.log("*************");
 }
+
+// 지역 변수의 값을 변경할 때
+function printOwing(invoice) {
+  let outstanding = 0;
+  printBanner();
+
+  // 미해결 채무(outStanding)를 계산한다.
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  recordDueDate(invoice);
+
+  printDetails(invoice, outstanding);
+}
+
+function recordDueDate(invoice) {
+  const today = Clock.today;
+  invoice.duDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function printDetails(invoice, outstanding) {
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`);
+}
+
+function printBanner() {
+  console.log("*************");
+  console.log("*** 고객 채무 ***");
+  console.log("*************");
+}
