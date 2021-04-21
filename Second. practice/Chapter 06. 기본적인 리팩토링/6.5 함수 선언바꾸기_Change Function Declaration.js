dcb7bc6804ceffe2f9169bbc40a24467e4431af5
choiseunghyeon <ket8780@gmail.com>
2021-04-21 22:31:08 +0900

    6.5 함수 선언바꾸기_Change Function Declation after refactoring

--- Second. practice/Chapter 06. 기본적인 리팩토링/6.5 함수 선언바꾸기_Change Function Declaration.js
+++ Second. practice/Chapter 06. 기본적인 리팩토링/6.5 함수 선언바꾸기_Change Function Declaration.js
@@ -1,9 +1,15 @@
 // 함수 이름 바꾸기 - 간단한 절차
-function circum(radius) {
+// 호출부도 동시에 바꾼다.
+function circumference(radius) {
   return 2 * Math.PI * radius;
 }
 
 // 함수 이름 바꾸기 - 마이그레이션 절차
+// 호출부를 하나씩 circum함수를 인라인하여 새로운 함수 circumference를 호출하도록 변경한다.
 function circum(radius) {
+  circumference(radius);
+}
+
+function circumference(radius) {
   return 2 * Math.PI * radius;
 }
