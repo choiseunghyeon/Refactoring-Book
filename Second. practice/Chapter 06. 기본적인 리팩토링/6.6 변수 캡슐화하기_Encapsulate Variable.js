ff2ac7ccba85a7b6a63759edee87e11827146131
choiseunghyeon <ket8780@gmail.com>
2021-04-21 23:13:34 +0900

    6.6 변수 캡슐화하기_Encapsulate Variable before refactoring

--- Second. practice/Chapter 06. 기본적인 리팩토링/6.6 변수 캡슐화하기_Encapsulate Variable.js
+++ Second. practice/Chapter 06. 기본적인 리팩토링/6.6 변수 캡슐화하기_Encapsulate Variable.js
@@ -12,3 +12,17 @@ function client() {
 
   setDefaultOwner({ firstName: "마틴", lastName: "파울러" });
 }
+
+// 값 캡슐화하기
+let defaultOwner = { firstName: "승현", lastName: "최" };
+function defaultOwner() {
+  return defaultOwner;
+}
+function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
+
+function client() {
+  const owner1 = defaultOwner();
+  owner1.lastName = "파슨스"; // 참조는 캡슐화 되었지만 값 내부 값 변경은 제어가 불가능
+}
