1a93365a4ba1f6492d89209a16b59041599f5f9e
choiseunghyeon <ket8780@gmail.com>
2021-04-21 22:08:24 +0900

    6.3 변수 추출하기_Extract Variable after refactoring

--- Second. practice/Chapter 06. 기본적인 리팩토링/6.3 변수 추출하기_Extract Variable.js
+++ Second. practice/Chapter 06. 기본적인 리팩토링/6.3 변수 추출하기_Extract Variable.js
@@ -1,7 +1,10 @@
 // 1
 function price(order) {
   // 가격(price) = 기본 가격 - 수량 할인 + 배송비
-  return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
 }
 
 // In Class
@@ -17,7 +20,16 @@ class Order {
     return this._data.itemPrice;
   }
 
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
   get price() {
-    return this.quantity * this.itemPrice - Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 + Math.min(this.quantity * this.itemPrice * 0.1, 100);
+    return this.basePrice - this.quantityDiscount + this.shipping;
   }
 }
