b89d6161d7e31047592901d258783968fa7a2e64
choiseunghyeon <ket8780@gmail.com>
2021-04-22 22:02:20 +0900

    6.10 여러 함수를 변환 함수로 묶기_Combine Functions into Transform after refactoring

--- Second. practice/Chapter 06. 기본적인 리팩토링/6.10 여러 함수를 변환 함수로 묶기_Combine Functions into Transform.js
+++ Second. practice/Chapter 06. 기본적인 리팩토링/6.10 여러 함수를 변환 함수로 묶기_Combine Functions into Transform.js
@@ -1,21 +1,30 @@
 const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
+function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, result.baseCharge - taxThreshold(result.year));
+  return result;
+}
+
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
 function client1() {
-  const aReading = acquireReading();
-  const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading);
+  const baseCharge = aReading.baseCharge;
 }
 
 function client2() {
-  const aReading = acquireReading();
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading);
+  const taxableCharge = aReading.taxableCharge;
 }
 
 function client3() {
-  const aReading = acquireReading();
-  const baseChargeAmount = calculateBaseCharge(aReading);
-
-  function calculateBaseCharge(aReading) {
-    return baseRate(aReading.month, aReading.year) * aReading.quantity;
-  }
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading);
+  const baseChargeAmount = aReading.baseCharge;
 }
