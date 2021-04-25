b2ac0a089976c09c20d24e8bd251e585423c11ec
choiseunghyeon <ket8780@gmail.com>
2021-04-25 22:39:28 +0900

    7.1 레코드 캡슐화하기_Encapsulate Record after refactoring

--- Second. practice/Chapter 07. 캡슐화/7.1 레코드 캡슐화하기_Encapsulate Record.js
+++ Second. practice/Chapter 07. 캡슐화/7.1 레코드 캡슐화하기_Encapsulate Record.js
@@ -1,7 +1,29 @@
-const organization = { name: "최승현", country: "KR" };
+const organization = new Organization({ name: "최승현", country: "KR" });
+function getOrganization() {
+  return organization;
+}
 
 function client() {
   let result = "";
-  result += `<h1>${organization.name}</h1>`;
-  organization.name = "홍길동";
+  result += `<h1>${getOrganization().name}</h1>`;
+  getOrganization().name = "홍길동";
+}
+
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(aString) {
+    this._name = aString;
+  }
+  get country() {
+    return this._country;
+  }
+  set country(aCountryCode) {
+    return (this._country = aCountryCode);
+  }
 }
