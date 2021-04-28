a0df51cf97ce18d9fb55dde759b0d7f8761ba3dc
choiseunghyeon <ket8780@gmail.com>
2021-04-28 20:46:04 +0900

    7.7 위임 숨기기 Hide Delegate before refactoring

---
+++ Second. practice/Chapter 07. 캡슐화/7.7 위임 숨기기_Hide Delegate.js
@@ -0,0 +1,35 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  get department() {
+    return this._department;
+  }
+  set department(arg) {
+    this._department = arg;
+  }
+}
+
+class Department {
+  get chargeCode() {
+    return this._chargeCode;
+  }
+  set chargeCode(arg) {
+    this._chargeCode = arg;
+  }
+  get manager() {
+    return this._manager;
+  }
+  set manager(arg) {
+    this._manager = arg;
+  }
+}
+
+// 어떤 사람이 속한 부서의 관리자를 알고 싶다.
+// 부서 클래스가 관리자 정보를 제공한다는 사실을 알아야한다.
+function client(params) {
+  const manager = aPerson.department.manager;
+}
