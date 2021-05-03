e0b6856dedafc38fefffd126715ed7ac8c3c1726
choiseunghyeon <ket8780@gmail.com>
2021-05-03 21:59:59 +0900

    8.1 함수 옮기기 Move Function _ class  before refactoring

--- Second. practice/Chapter 08. 기능 이동/8.1 함수 옮기기_Move Function.js
+++ Second. practice/Chapter 08. 기능 이동/8.1 함수 옮기기_Move Function.js
@@ -1,3 +1,4 @@
+// 중첩 함수를 최상위로 옮기기
 function trackSummary(points) {
   const totalTime = calculateTime();
   const pace = totalTime / 60 / totalDistance(points);
@@ -30,3 +31,26 @@ function distance(p1, p2) {
 function radians(degrees) {
   return (degrees * Math.PI) / 180;
 } // 라디안 값으로 변환
+
+// 다른 클래스로 옮기기
+class Acount {
+  get bankCharge() {
+    // 은행 이자 계산
+    let result = 4.5;
+    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() {
+    // 초과 인출 이자 계산
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this._daysOverdrawn <= 7) return baseCharge;
+      else return baseCharge + this._daysOverdrawn - 7 * 0.85;
+    } else {
+      return this._daysOverdrawn * 1.75;
+    }
+  }
+}
+
+class AccountType {}
