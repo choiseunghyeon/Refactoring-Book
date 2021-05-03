3f59a63609920284bcfcf073e7953d72c4c6dd5a
choiseunghyeon <ket8780@gmail.com>
2021-05-03 22:05:11 +0900

    8.1 함수 옮기기 Move Function _ class after refactoring

--- Second. practice/Chapter 08. 기능 이동/8.1 함수 옮기기_Move Function.js
+++ Second. practice/Chapter 08. 기능 이동/8.1 함수 옮기기_Move Function.js
@@ -37,20 +37,20 @@ class Acount {
   get bankCharge() {
     // 은행 이자 계산
     let result = 4.5;
-    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    if (this.daysOverdrawn > 0) result += this.type.overdraftCharge(this.daysOverdrawn);
     return result;
-  }
-
-  get overdraftCharge() {
-    // 초과 인출 이자 계산
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + this._daysOverdrawn - 7 * 0.85;
-    } else {
-      return this._daysOverdrawn * 1.75;
-    }
   }
 }
 
-class AccountType {}
+class AccountType {
+  get overdraftCharge(daysOverdrawn) {
+    // 초과 인출 이자 계산
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (daysOverdrawn <= 7) return baseCharge;
+      else return baseCharge + daysOverdrawn - 7 * 0.85;
+    } else {
+      return daysOverdrawn * 1.75;
+    }
+  }
+}
