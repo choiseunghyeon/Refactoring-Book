e26b20edd9a3700ddca83a3be8dba90d8cf47a0f
choiseunghyeon <ket8780@gmail.com>
2021-05-04 22:26:54 +0900

    8.2 필드 옮기기 Move Field before refactoring

--- Second. practice/Chapter 08. 기능 이동/8.2 필드 옮기기_Move Field.js
+++ Second. practice/Chapter 08. 기능 이동/8.2 필드 옮기기_Move Field.js
@@ -34,3 +34,23 @@ class CustomerContract {
     this._discountRate = arg;
   }
 }
+
+// 공유 객체로 이동하기
+// 이자율이 계좌별로 설정되고 있지만 계좌 종류에 따라 정해지도록 수정
+class Account {
+  constructor(number, type, insertRate) {
+    this._number = number;
+    this._type = type;
+    this._insertRate = insertRate;
+  }
+
+  get insertRate() {
+    return this._insertRate;
+  }
+}
+
+class AccountType {
+  constructor(nameString) {
+    this._name = nameString;
+  }
+}
