2ab2afc37fcd267def6f73a4b370a3bf8353edfe
choiseunghyeon <ket8780@gmail.com>
2021-05-04 23:08:36 +0900

    8.3 문장을 함수로 옮기기 Move Statements into Function after refactoring

--- Second. practice/Chapter 08. 기능 이동/8.3 문장을 함수로 옮기기_Move Statements into Function.js
+++ Second. practice/Chapter 08. 기능 이동/8.3 문장을 함수로 옮기기_Move Statements into Function.js
@@ -2,18 +2,16 @@ function renderPerson(outStream, person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>제목: ${person.photo.title}</p>`);
   result.push(emitPhotoData(person.photo));
   return result.join("\n");
 }
 
 function photoDiv(p) {
-  return ["<div>", `<p>제목: ${p.title}</p>`, emitPhotoData(p), "</div>"].join("\n");
+  return ["<div>", emitPhotoData(p), "</div>"].join("\n");
 }
 
-function emitPhotoData(aPhoto) {
-  const result = [];
-  result.push(`<p>위치: ${aPhoto.location}</p>`);
-  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-  return result.join("\n");
+function emitPhotoData(p) {
+  return [`<p>제목: ${p.title}</p>`, `<p>위치: ${aPhoto.location}</p>`, `<p>날짜: ${aPhoto.date.toDateString()}</p>`].join("\n");
 }
+
+function emitPhotoData(aPhoto) {}
