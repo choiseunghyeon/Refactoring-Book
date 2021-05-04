d9a822f99ddf5c2c56a23fdaa9f0917f1d0f5b62
choiseunghyeon <ket8780@gmail.com>
2021-05-04 22:23:00 +0900

    8.2 필드 옮기기 Move Field after refactoring

--- Second. practice/Chapter 08. 기능 이동/8.2 필드 옮기기_Move Field.js
+++ Second. practice/Chapter 08. 기능 이동/8.2 필드 옮기기_Move Field.js
@@ -2,24 +2,35 @@
 class Customer {
   constructor(name, discountRate) {
     this._name = name;
-    this._discountRate = discountRate;
     this._contract = new CustomerContract(dateToday());
+    this._setDiscountRate(discountRate);
+  }
+
+  get discountRate() {
+    return this._contract.discountRate;
+  }
+  _setDiscountRate(aNumber) {
+    this._contract.discountRate = aNumber;
+  }
+  becomePreferred() {
+    this._setDiscountRate((this._contract.discountRate += 0.03));
+    // 기타
+  }
+  applyDiscount(amount) {
+    return amount.subtract(amount.multiply(this._contract.discountRate));
+  }
+}
+
+class CustomerContract {
+  constructor(startDate, discountRate) {
+    this._startDate = startDate;
+    this._discountRate = discountRate;
   }
 
   get discountRate() {
     return this._discountRate;
   }
-  becomePreferred() {
-    this._discountRate += 0.03;
-    // 기타
-  }
-  applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this._discountRate));
-  }
-}
-
-class CustomerContract {
-  constructor(startDate) {
-    this._startDate = startDate;
+  set discountRate(arg) {
+    this._discountRate = arg;
   }
 }
