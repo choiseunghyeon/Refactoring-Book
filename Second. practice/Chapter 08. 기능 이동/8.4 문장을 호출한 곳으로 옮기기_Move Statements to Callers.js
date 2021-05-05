055de27daf528091f8bce3214ad449d25f6cdbca
choiseunghyeon <ket8780@gmail.com>
2021-05-05 22:51:28 +0900

    8.4 문장을 호출한 곳으로 옮기기 Move Statements to Callers after refactoring

--- Second. practice/Chapter 08. 기능 이동/8.4 문장을 호출한 곳으로 옮기기_Move Statements to Callers.js
+++ Second. practice/Chapter 08. 기능 이동/8.4 문장을 호출한 곳으로 옮기기_Move Statements to Callers.js
@@ -4,6 +4,7 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>위치: ${person.photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -12,6 +13,7 @@ function listRecentPhotos(outStream, photos) {
     .forEach(p => {
       outStream.write("<div>\n");
       emitPhotoData(outStream, p);
+      outStream.write(`<div>위치: ${p.location}</div>\n`);
       outStream.write("</div>\n");
     });
 }
@@ -19,5 +21,4 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>제목: ${photo.title}</p>\n`);
   outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
