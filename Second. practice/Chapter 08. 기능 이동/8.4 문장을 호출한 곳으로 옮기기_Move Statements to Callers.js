38cd6e23ca7659ce9b1fcc3a95e3b7b4f53a61e8
choiseunghyeon <ket8780@gmail.com>
2021-05-05 22:46:00 +0900

    8.4 문장을 호출한 곳으로 옮기기 Move Statements to Callers before refactoring

---
+++ Second. practice/Chapter 08. 기능 이동/8.4 문장을 호출한 곳으로 옮기기_Move Statements to Callers.js
@@ -0,0 +1,23 @@
+// 호출자가 둘 뿐인 단순한 상황
+// listRecentPhotos에서 location을 다르게 표현해야 한다.
+function renderPerson(outStream, person) {
+  outStream.write(`<p>${person.name}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
+}
+
+function listRecentPhotos(outStream, photos) {
+  photos
+    .filter(p => p.date > recentDateCutoff())
+    .forEach(p => {
+      outStream.write("<div>\n");
+      emitPhotoData(outStream, p);
+      outStream.write("</div>\n");
+    });
+}
+
+function emitPhotoData(outStream, photo) {
+  outStream.write(`<p>제목: ${photo.title}</p>\n`);
+  outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
+  outStream.write(`<p>위치: ${photo.location}</p>\n`);
+}
