c5b7d4177ab4de7a501a8ca6857ffc49d24705b5
choiseunghyeon <ket8780@gmail.com>
2021-05-06 22:07:53 +0900

    8.7 반복문 쪼개기 Split Loop after refactoring

--- Second. practice/Chapter 08. 기능 이동/반복문 쪼개기_Split Loop.js
+++ Second. practice/Chapter 08. 기능 이동/반복문 쪼개기_Split Loop.js
@@ -1,8 +1,12 @@
 function example(params) {
   let youngest = people[0] ? people[0].age : Infinity;
   let totalSalary = 0;
+
   for (const p of people) {
     if (p.age < youngest) youngest = p.age;
+  }
+
+  for (const p of people) {
     totalSalary += p.salary;
   }
 
