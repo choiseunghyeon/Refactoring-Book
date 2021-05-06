9b0ecf5d2737d51d210dbe675ba696be7a8a31e7
choiseunghyeon <ket8780@gmail.com>
2021-05-06 21:47:50 +0900

    8.6 문장 슬라이드하기 Slide Statements before refactoring

---
+++ Second. practice/Chapter 08. 기능 이동/8.6 문장 슬라이드하기_Slide Statements.js
@@ -0,0 +1,13 @@
+const pricingPlan = retrievePricingPlan();
+const order = retreiveOrder();
+const baseCharge = pricingPlan.base;
+let charge;
+const chargePerUnit = pricingPlan.unit;
+const units = order.units;
+let discount;
+charge = baseCharge + units * chargePerUnit;
+let discountableunits = Math.max(units - pricingPlan.discountThresholde, 0);
+discount = discountableunits * pricingPlan.discountFactor;
+if (order.isRepeat) discount += 20;
+charge = charge - discount;
+chargeOrder(charge);
