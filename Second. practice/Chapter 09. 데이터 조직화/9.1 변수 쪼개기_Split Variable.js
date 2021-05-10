39ffdf0b65293257b4695682382223304c6d456e
choiseunghyeon <ket8780@gmail.com>
2021-05-10 20:37:54 +0900

    9.1 변수 쪼개기 Split Variable after refactoring

--- Second. practice/Chapter 09. 데이터 조직화/9.1 변수 쪼개기_Split Variable.js
+++ Second. practice/Chapter 09. 데이터 조직화/9.1 변수 쪼개기_Split Variable.js
@@ -1,15 +1,15 @@
 // 해기스라는 음식이 다른 지역으로 전파된 거리를 구하는 코드
 function distanceTravelled(scenario, time) {
-  let reulst;
-  let acc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
-  let primaryTime = Math.min(time, scenario.delay);
-  result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
-  let secondaryTime = time - scenario.delay;
+  let result;
+  const primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
+  const primaryTime = Math.min(time, scenario.delay);
+  result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
+  const secondaryTime = time - scenario.delay;
   if (secondaryTime > 0) {
     //두 번째 힘을 반영해 다시 계산
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-    result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+    let primaryVelocity = primaryAcceleration * scenario.delay;
+    let secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
   }
   return result;
 }
