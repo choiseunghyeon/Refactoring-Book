281d0405c8c7f1e2a7b3622134a47867343dcb48
choiseunghyeon <ket8780@gmail.com>
2021-05-10 20:47:27 +0900

    9.1 변수 쪼개기 Split Variable after refactoring2

--- Second. practice/Chapter 09. 데이터 조직화/9.1 변수 쪼개기_Split Variable.js
+++ Second. practice/Chapter 09. 데이터 조직화/9.1 변수 쪼개기_Split Variable.js
@@ -16,7 +16,8 @@ function distanceTravelled(scenario, time) {
 
 // 입력 매개변수의 값을 수정할 때
 function discount(inputValue, quantity) {
-  if (inputValue > 50) inputValue = inputValue - 2;
-  if (quantity > 100) inputValue = inputValue - 1;
-  return inputValue;
+  let result = inputValue;
+  if (inputValue > 50) result = result - 2;
+  if (quantity > 100) result = result - 1;
+  return result;
 }
