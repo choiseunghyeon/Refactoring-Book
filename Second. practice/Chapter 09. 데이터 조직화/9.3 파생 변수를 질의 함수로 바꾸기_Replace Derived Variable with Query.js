246d0b527286c2dd369a5fb1880c911645e47671
choiseunghyeon <ket8780@gmail.com>
2021-05-11 22:04:58 +0900

    9.3 파생 변수를 질의 함수로 바꾸기 Replace Derived Variable with query after refactoring

--- Second. practice/Chapter 09. 데이터 조직화/9.3 파생 변수를 질의 함수로 바꾸기_Replace Derived Variable with Query.js
+++ Second. practice/Chapter 09. 데이터 조직화/9.3 파생 변수를 질의 함수로 바꾸기_Replace Derived Variable with Query.js
@@ -1,10 +1,9 @@
 class ProductionPlan {
   get production() {
-    return this._production;
+    return this._adjustment.reduce((sum, a) => sum + a.amount, 0);
   }
+
   applyAdjustment(anAdjustment) {
     this._adjustment.push(anAdjustment);
-    // 데이터 중복 adjustment 적용에서 직접적인 관련이 없이 누적 값 production 갱신 중
-    this._production += anAdjustment.amount;
   }
 }
