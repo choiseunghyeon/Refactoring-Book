08833021b6690fd31bb1df199bca1d17bf3edec2
choiseunghyeon <ket8780@gmail.com>
2021-05-11 21:59:57 +0900

    9.2 필드 이름 바꾸기 Rename Field after refactoring

--- Second. practice/Chapter 09. 데이터 조직화/9.2 필드 이름 바꾸기_Rename Field.js
+++ Second. practice/Chapter 09. 데이터 조직화/9.2 필드 이름 바꾸기_Rename Field.js
@@ -1,2 +1,23 @@
 // name을 title로 바꾸고 싶다.
-const organization = { name: "애크미 구스베리", country: "GB" };
+
+class Organization {
+  constructor(data) {
+    this._title = data.title;
+    this._country = data.country;
+  }
+
+  get title() {
+    return this._title;
+  }
+  set title(aString) {
+    this._title = aString;
+  }
+  get country() {
+    return this._country;
+  }
+  set country(aCountryCode) {
+    this._country = aCountryCode;
+  }
+}
+
+const organization = new Organization({ title: "애크미 구스베리", country: "GB" });
