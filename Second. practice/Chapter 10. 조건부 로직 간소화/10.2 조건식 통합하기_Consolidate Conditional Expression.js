b31bd08038802f33f5bdd8a569867e647f0ad815
choiseunghyeon <ket8780@gmail.com>
2021-05-13 21:54:29 +0900

    10.2 조건식 통합하기 Consolidate Conditional Expression after refactoring

--- Second. practice/Chapter 10. 조건부 로직 간소화/10.2 조건식 통합하기_Consolidate Conditional Expression.js
+++ Second. practice/Chapter 10. 조건부 로직 간소화/10.2 조건식 통합하기_Consolidate Conditional Expression.js
@@ -1,18 +1,18 @@
 // or 사용하기
 function disabliltyAmount(anEmployee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
-  if (anEmployee.isPartTime) return 0;
+  if (isNotEligibleForDisability()) return 0;
+
+  function isNotEligibleForDisability() {
+    return anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime;
+  }
   //장애 수당 계산
 }
 
 // and 사용하기
 
 function eg(anEmployee) {
-  if (anEmployee.onVacation) {
-    if (anEmployee.seniority > 10) {
-      return 1;
-    }
+  if (anEmployee.onVacation && anEmployee.seniority > 10) {
+    return 1;
   }
   return 0.5;
 }
