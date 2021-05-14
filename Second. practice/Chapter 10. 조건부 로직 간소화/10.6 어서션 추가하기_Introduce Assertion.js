44759dcefa7cb2de403d7421fbb4eacb2f0fc61f
choiseunghyeon <ket8780@gmail.com>
2021-05-14 23:39:50 +0900

    10.6 어서션 추가하기 Introduce Assetion after refactoring

--- Second. practice/Chapter 10. 조건부 로직 간소화/10.6 어서션 추가하기_Introduce Assertion.js
+++ Second. practice/Chapter 10. 조건부 로직 간소화/10.6 어서션 추가하기_Introduce Assertion.js
@@ -1,6 +1,14 @@
 class Customer {
   applyDiscount(aNumber) {
     // 할인율이 항상 양수라는 가정이 깔려 있다.
-    return this.discountRate ? aNumber - this.discountRate * aNumber : aNumber;
+    if (!this.discountRate) return aNumber;
+    else {
+      return aNumber - this.discountRate * aNumber;
+    }
+  }
+
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this._discountRate = aNumber;
   }
 }
