bb70f80aad671d91b54142941d25d6f92ab6a28f
choiseunghyeon <ket8780@gmail.com>
2021-05-14 22:13:19 +0900

    10.3 중첩 조건문을 보호 구문으로 바꾸기 Replace Nested Conditional with Guard Clauses after refactoring2

--- Second. practice/Chapter 10. 조건부 로직 간소화/10.3 중첩 조건문을 보호 구문으로 바꾸기_Replace Nested Conditional with Guard Clauses.js
+++ Second. practice/Chapter 10. 조건부 로직 간소화/10.3 중첩 조건문을 보호 구문으로 바꾸기_Replace Nested Conditional with Guard Clauses.js
@@ -11,11 +11,6 @@ function payAmount(employee) {
 
 // 조건 반대로 만들기
 function adjustedCapital(anInstrument) {
-  let result = 0;
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-    }
-  }
-  return result;
+  if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
+  return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
