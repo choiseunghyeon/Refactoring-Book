a6350b01bd84e44207e066da2338247ca62d2a68
choiseunghyeon <ket8780@gmail.com>
2021-05-14 23:50:13 +0900

    10.7 제어 플래그를 탈출문으로 바꾸기 Replace Control Flag with Break after refactoring

--- Second. practice/Chapter 10. 조건부 로직 간소화/10.7 제어 플래그를 탈출문으로 바꾸기_Replace Control Flag with Break.js
+++ Second. practice/Chapter 10. 조건부 로직 간소화/10.7 제어 플래그를 탈출문으로 바꾸기_Replace Control Flag with Break.js
@@ -1,15 +1,22 @@
 // 사람 목록을 흝으면서 악당을 찾는다.
 
-let found = false;
-for (const p of people) {
-  if (!found) {
+// 생략
+checkForMiscreants(people);
+// 생략
+function checkForMiscreants(people) {
+  for (const p of people) {
     if (p === "조커") {
       sendAlert();
-      found = true;
+      return;
     }
     if (p === "사루만") {
       sendAlert();
-      found = true;
+      return;
     }
   }
+
+  /*
+   다음과 같이 변경하는것도 깔끔하다.
+   if (people.some(p => ["조커", "사루만"].includes(p))) sendAlert();
+   */
 }
