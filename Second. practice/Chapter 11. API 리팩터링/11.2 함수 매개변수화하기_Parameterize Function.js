3f88bcbfdcf983f01b4b4f7533da51629e4f05d9
choiseunghyeon <ket8780@gmail.com>
2021-05-18 23:43:09 +0900

    11.2 함수 매개변수화하기 Parameterize Function after refactoring

--- Second. practice/Chapter 11. API 리팩터링/11.2 함수 매개변수화하기_Parameterize Function.js
+++ Second. practice/Chapter 11. API 리팩터링/11.2 함수 매개변수화하기_Parameterize Function.js
@@ -1,18 +1,19 @@
 // 간단한 예시
-function tenPercentRaise(aPerson) {
-  aPerson.salary = aPerson.multiply(1.1);
-}
 
-function fivePercentRaise(aPerson) {
-  aPerson.salary = aPerson.multiply(1.05);
+function raise(aPerson, factor) {
+  aPerson.salary = aPerson.multiply(1 + factor);
 }
 
 // 간단하지 않은 예시
 
 function baseCharge(usage) {
   if (usage < 0) return usd(0);
-  const amount = bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+  const amount = withinBand(usage, 0, 100) * 0.03 + withinBand(usage, 100, 200) * 0.05 + withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
+}
+
+function withinBand(usage, bottom, top) {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function bottomBand(usage) {
