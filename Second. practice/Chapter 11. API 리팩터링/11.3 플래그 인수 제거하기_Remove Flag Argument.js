cc38b593163aacde78201f89cfdf47dde9d92e4f
choiseunghyeon <ket8780@gmail.com>
2021-05-19 20:23:23 +0900

    11.3 플래그 인수 제거하기 Remove Flag Argument after refactoring

--- Second. practice/Chapter 11. API 리팩터링/11.3 플래그 인수 제거하기_Remove Flag Argument.js
+++ Second. practice/Chapter 11. API 리팩터링/11.3 플래그 인수 제거하기_Remove Flag Argument.js
@@ -1,23 +1,23 @@
-function deliveryDate(anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
-    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-  } else {
-    let deliveryTime;
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
+function regularDeliveryDate(anOrder) {
+  let deliveryTime;
+  if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+  else deliveryTime = 4;
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
+function rushDeliveryDate(anOrder) {
+  let deliveryTime;
+  if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+  else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else deliveryTime = 3;
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
 function client1() {
-  aShipment.deliveryDate = deliveryDate(anOrder, true);
+  aShipment.deliveryDate = rushDeliveryDate(anOrder, true);
 }
 
 function client2() {
-  aShipment.deliveryDate = deliveryDate(anOrder, false);
+  aShipment.deliveryDate = regularDeliveryDate(anOrder, false);
 }
