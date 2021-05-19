e35809cd607f87df15df3b2b477d78e54def23ef
choiseunghyeon <ket8780@gmail.com>
2021-05-20 00:26:45 +0900

    11.8 생성자를 팩터리 함수로 바꾸기 Replace Constrcutor with Factory Function after refactoring

--- Second. practice/Chapter 11. API 리팩터링/11.8 생성자를 팩터리 함수로 바꾸기_Replace Constructor with Factory Function.js
+++ Second. practice/Chapter 11. API 리팩터링/11.8 생성자를 팩터리 함수로 바꾸기_Replace Constructor with Factory Function.js
@@ -16,9 +16,17 @@ class Employee {
 }
 
 function client1() {
-  const candidate = new Employee(document.name, document.empType);
+  const candidate = createEmployee(document.name, document.empType);
 }
 
 function client2() {
-  const leadEnginner = new Employee(document.leadEnginner, "E");
+  const leadEnginner = createEngineer(document.leadEnginner);
+}
+
+function createEmployee(name, typeCode) {
+  return new Employee(name, typeCode);
+}
+
+function createEngineer(name) {
+  return new Employee(name, "E");
 }
