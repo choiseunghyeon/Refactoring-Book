0c50dd6c7453c29b1ab296208aa332554a388351
choiseunghyeon <ket8780@gmail.com>
2021-05-19 20:29:13 +0900

    11.3 플래그 인수 제거하기 Remove Flag Argument before refactoring2

--- Second. practice/Chapter 11. API 리팩터링/11.3 플래그 인수 제거하기_Remove Flag Argument.js
+++ Second. practice/Chapter 11. API 리팩터링/11.3 플래그 인수 제거하기_Remove Flag Argument.js
@@ -21,3 +21,19 @@ function client1() {
 function client2() {
   aShipment.deliveryDate = regularDeliveryDate(anOrder, false);
 }
+
+// 매개변수를 까다로운 방식으로 사용할 때 단순히 래핑할 수도 있다.
+function deliveryDate(anOrder, isRush) {
+  let result;
+  let deliveryTime;
+  if (anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT") deliveryTime = isRush ? 1 : 2;
+  else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH") {
+    deliveryTime = 2;
+    if (anOrder.deliveryState === "NH" && !isRush) deliveryTime = 3;
+  } else if (isRush) deliveryTime = 3;
+  else if (anOrder.deliveryState === "ME") deliveryTime = 3;
+  else deliveryTime = 4;
+  result = anOrder.placedOn.plusDays(2 + deliveryTime);
+  if (isRush) result = result.minusDays(1);
+  return result;
+}
