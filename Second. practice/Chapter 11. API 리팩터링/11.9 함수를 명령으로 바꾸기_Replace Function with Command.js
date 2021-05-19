a007edeb6dbd4b576218052bb07fa6005b9b4edb
choiseunghyeon <ket8780@gmail.com>
2021-05-20 00:47:48 +0900

    11.9 함수를 명령으로 바꾸기 Replace Function with Command after refactoring

--- Second. practice/Chapter 11. API 리팩터링/11.9 함수를 명령으로 바꾸기_Replace Function with Command.js
+++ Second. practice/Chapter 11. API 리팩터링/11.9 함수를 명령으로 바꾸기_Replace Function with Command.js
@@ -1,18 +1,33 @@
 function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer(candidate, medicalExam, scoringGuide).excute();
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+class Scorer {
+  constructor(candidate) {
+    this._candidate = candidate;
+    this._medicalExam = medicalExam;
+    this._scoringGuide = scoringGuide;
   }
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWIthLowCerification(candidate.originState)) {
-    certificationGrade = "low";
-    result -= 5;
+  excute() {
+    this._result = 0;
+    this._healthLevel = 0;
+    this._highMedicalRiskFlag = false;
+
+    this.scoreSmoking();
+    let certificationGrade = "regular";
+    if (this._scoringGuide.stateWIthLowCerification(this._candidate.originState)) {
+      certificationGrade = "low";
+      this._result -= 5;
+    }
+    // 비슷한 코드 이어짐
+    this._result -= Math.max(this._healthLevel - 5, 0);
+    return this._result;
   }
-  // 비슷한 코드 이어짐
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
+
+  scoreSmoking() {
+    if (this._medicalExam.isSmoker) {
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
+    }
+  }
 }
