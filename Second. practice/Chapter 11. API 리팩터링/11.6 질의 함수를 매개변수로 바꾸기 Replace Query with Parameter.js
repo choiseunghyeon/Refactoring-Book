db9b94a2eb444b5aa3d456e1b8b9f3b693f68d1e
choiseunghyeon <ket8780@gmail.com>
2021-05-19 23:22:59 +0900

    11.6 질의 함수를 매개변수로 바꾸기 Replace Query with Parameter after refactoring

--- Second. practice/Chapter 11. API 리팩터링/11.6 질의 함수를 매개변수로 바꾸기 Replace Query with Parameter.js
+++ Second. practice/Chapter 11. API 리팩터링/11.6 질의 함수를 매개변수로 바꾸기 Replace Query with Parameter.js
@@ -1,16 +1,15 @@
 // 실내온도 제어 시스템 - 사용자는 온도조절기로 온도를 설정할 수 있지만, 목표 온도는 난방 계획에서 정한 범위에서만 선택할 수 있다.
 
 class HeatingPlan {
-  get targetTemperature() {
-    // thermostat - 전역 객체
-    if (thermostat.selectedTemperature > this._max) return this._max;
-    else if (thermostat.selectedTemperature < this._min) return this._min;
-    else return thermostat.selectedTemperature;
+  targetTemperature(selectedTemperature) {
+    if (selectedTemperature > this._max) return this._max;
+    else if (selectedTemperature < this._min) return this._min;
+    else return selectedTemperature;
   }
 }
 
 function client() {
-  if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
-  else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+  if (thePlan.targetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) setToHeat();
+  else if (thePlan.targetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature) setToCool();
   else setOff();
 }
