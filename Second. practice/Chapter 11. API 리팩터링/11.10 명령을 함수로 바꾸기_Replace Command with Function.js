1e896272818f402e4ee29d04474eedc0e25f848a
choiseunghyeon <ket8780@gmail.com>
2021-05-20 01:00:46 +0900

    11.10 명령을 함수로 바꾸기 Replace Command with Function after refactoring

--- Second. practice/Chapter 11. API 리팩터링/11.10 명령을 함수로 바꾸기_Replace Command with Function.js
+++ Second. practice/Chapter 11. API 리팩터링/11.10 명령을 함수로 바꾸기_Replace Command with Function.js
@@ -1,17 +1,8 @@
-class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-  get charge() {
-    return this._baseCharge + this._provider.connectionCharge;
-  }
+function client() {
+  const monthCharge = charge(customer, usage, provider);
 }
 
-function client() {
-  const monthCharge = new ChargeCalculator(customer, usage, provider).charge;
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
