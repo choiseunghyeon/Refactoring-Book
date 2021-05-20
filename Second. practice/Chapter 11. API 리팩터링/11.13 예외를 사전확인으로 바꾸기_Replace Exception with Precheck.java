4d4c42dc4e03516f03e7036fde313ad3018559f7
choiseunghyeon <ket8780@gmail.com>
2021-05-20 20:23:03 +0900

    11.13 예외를 사전확인으로 바꾸기 Replace Exception with Precheck before refactoring

---
+++ Second. practice/Chapter 11. API 리팩터링/11.13 예외를 사전확인으로 바꾸기_Replace Exception with Precheck.java
@@ -0,0 +1,19 @@
+import java.util.NoSuchElementException;
+
+class ResourcePool {
+    public Resource get() {
+        Resource result;
+        try {
+            result = available.pop();
+            allocated.add(result);
+        } catch (NoSuchElementException e) {
+            result = Resource.create();
+            allocated.add(result);
+        }
+        return result;
+    }
+
+    private Deque<Resource> available;
+    private List<Resource> allocated;
+
+}
