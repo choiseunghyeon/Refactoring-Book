8dde00153320ece158208e37ec86782c7d9acaf4
choiseunghyeon <ket8780@gmail.com>
2021-05-23 00:05:04 +0900

    12.1 메서드 올리기 Pull up method after refactoring

--- Second. practice/Chapter 12. 상속 다루기/12.1 메서드 올리기_Pull Up Method.js
+++ Second. practice/Chapter 12. 상속 다루기/12.1 메서드 올리기_Pull Up Method.js
@@ -1,13 +1,9 @@
-class Party {}
-
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
 
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
-}
+class Employee extends Party {}
+
+class Department extends Party {}
