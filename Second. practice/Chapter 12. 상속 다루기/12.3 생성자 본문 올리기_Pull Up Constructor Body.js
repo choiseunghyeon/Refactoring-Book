02f93680e46b479018165c4d2a0abf6385ee2094
choiseunghyeon <ket8780@gmail.com>
2021-05-23 00:59:12 +0900

    12.3 생성자 본문 올리기 Pull up constructor body after refactoring2

--- Second. practice/Chapter 12. 상속 다루기/12.3 생성자 본문 올리기_Pull Up Constructor Body.js
+++ Second. practice/Chapter 12. 상속 다루기/12.3 생성자 본문 올리기_Pull Up Constructor Body.js
@@ -32,14 +32,19 @@ class Employee {
   assignCar() {
     /* ... */
   }
+
+  finishConstruction() {
+    if (this.isPrivileged) this.assignCar();
+  }
 }
 
 class Manager extends Employee {
   constructor(name, grade) {
     super(name);
     this._grade = grade;
-    if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행
+    this.finishConstruction(); // 모든 서브클래스가 수행
   }
+
   get isPrivileged() {
     return this._grade > 4;
   }
