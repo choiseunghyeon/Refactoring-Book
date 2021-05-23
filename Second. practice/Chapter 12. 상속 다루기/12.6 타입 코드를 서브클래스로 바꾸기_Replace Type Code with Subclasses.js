a41b39274322c8f1b91decff8fbcddf6193a9c2f
choiseunghyeon <ket8780@gmail.com>
2021-05-23 16:16:36 +0900

    12.6 타입 코드를 서브클래스로 바꾸기 Replace Type Code with Subclasses before refactoring2

--- Second. practice/Chapter 12. 상속 다루기/12.6 타입 코드를 서브클래스로 바꾸기_Replace Type Code with Subclasses.js
+++ Second. practice/Chapter 12. 상속 다루기/12.6 타입 코드를 서브클래스로 바꾸기_Replace Type Code with Subclasses.js
@@ -35,3 +35,33 @@ function createEmployee(name, type) {
       throw new Error(`${type}라는 직원 유형은 없습니다.`);
   }
 }
+
+// 간접 상속할 때
+// 직원의 서브클래스로 '아르바이트', '정직원' 클래스가 있어서 직접 상속으로 타입 코드 문제에 대처할 수 없다. 그리고 직원 유형을 변경하는 기능을 유지하고 싶기에 직접 상속을 사용하지 않는다.
+
+class Employee {
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg) {
+    if (!["engineer", "manager", "salesperson"].includes(arg)) throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+  }
+
+  get type() {
+    return this._type;
+  }
+  set type(arg) {
+    this._type = arg;
+  }
+
+  get capitalizedType() {
+    return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+  }
+
+  toString() {
+    return `${this.name} (${this.capitalizedType})`;
+  }
+}
