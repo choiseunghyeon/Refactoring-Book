0fc382768f359df10a66c3dae087bfd57cfd7013
choiseunghyeon <ket8780@gmail.com>
2021-05-25 00:10:04 +0900

    12.10 서브클래스를 위임으로 바꾸기 Replace Subclass with  Delegate after refactoring

--- Second. practice/Chapter 12. 상속 다루기/12.10 서브클래스를 위임으로 바꾸기_Replace Subclass with Delegate.js
+++ Second. practice/Chapter 12. 상속 다루기/12.10 서브클래스를 위임으로 바꾸기_Replace Subclass with Delegate.js
@@ -5,40 +5,63 @@ class Booking {
   }
 
   get hasTalkback() {
-    return this._show.hasOwnProperty("talkback") && !this.isPeakDay;
+    return this._premiumDelegate ? this._premiumDelegate.hasTalkback : this._show.hasOwnProperty("talkback") && !this.isPeakDay;
   }
 
   get basePrice() {
+    return this._premiumDelegate ? this._premiumDelegate.basePrice : this._prevateBasePrice;
+  }
+
+  get _privateBasePrice() {
     let result = this._show.price;
     if (this.isPeakDay) result += Math.round(result * 0.15);
     return result;
   }
+
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
+
+  get hasDinner() {
+    return this._premiumDelegate ? this._premiumDelegate.hasDinner : undefined;
+  }
 }
 
 // 슈퍼클래스를 상속해 많은 걸 변경한다. 메서드 일부를 오버라이드 하거나 필요한 메서드를 몇개 추가 한다.
-class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
+
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
     this._extras = extras;
   }
 
   get hasTalkback() {
-    return this._show.hasOwnProperty("talkback");
+    return this._host._show.hasOwnProperty("talkback");
   }
 
   get basePrice() {
-    return Math.round(super.basePrice + this._extras.premiumFee);
+    return Math.round(this._host._prevateBasePrice + this._extras.premiumFee);
   }
 
   get hasDinner() {
-    return this._extras.hasOwnProperty("dinner") && !this.isPeakDay;
+    return this._extras.hasOwnProperty("dinner") && !this._host.isPeakDay;
   }
 }
 
 function client1() {
-  const aBooking = new Booking(show, date);
+  const aBooking = createBooking(show, date);
 }
 
 function client2() {
-  const aBooking = new PremiumBooking(show, date, extras);
+  const aBooking = createPremiumBooking(show, date, extras);
+}
+
+function createPremiumBooking() {
+  const result = new Booking(show, date, extras);
+  result._bePremium(extras);
+  return result;
+}
+
+function createBooking(show, date) {
+  return new Booking(show, date);
 }
