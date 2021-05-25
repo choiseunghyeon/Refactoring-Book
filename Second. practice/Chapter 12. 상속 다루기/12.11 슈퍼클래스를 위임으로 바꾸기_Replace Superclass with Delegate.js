104b748b29521f97b4d043d3be7e81c6de495c01
choiseunghyeon <ket8780@gmail.com>
2021-05-25 21:31:52 +0900

    12.11 슈퍼클래스를 위임으로 바꾸기 Replace Superclass with Delegate after refactoring

--- Second. practice/Chapter 12. 상속 다루기/12.11 슈퍼클래스를 위임으로 바꾸기_Replace Superclass with Delegate.js
+++ Second. practice/Chapter 12. 상속 다루기/12.11 슈퍼클래스를 위임으로 바꾸기_Replace Superclass with Delegate.js
@@ -16,12 +16,22 @@ class CatalogItem {
   }
 }
 
-class Scroll extends CatalogItem {
-  constructor(id, title, tags, dateLastCleaned) {
-    super(id, title, tags);
+class Scroll {
+  constructor(id, dateLastCleaned, catalogID, catalog) {
+    this._id = id;
+    this._catalogItem = catalog.get(catalogID);
     this._lastCleaned = dateLastCleaned;
   }
 
+  get id() {
+    return this._id;
+  }
+  get title() {
+    return this._catalogItem.title;
+  }
+  hasTag(aString) {
+    return this._catalogItem.hasTag(aString);
+  }
   needsCleaning(targetDate) {
     const threshold = this.hasTag("revered") ? 700 : 1500;
     return this.daysSinceLastCleaning(targetDate) > threshold;
@@ -30,4 +40,8 @@ class Scroll extends CatalogItem {
   daysSinceLastCleaning(targetDate) {
     return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
   }
+}
+
+function client() {
+  const scrolls = aDocument.map(record => new Scroll(record.id, LocalDate.parse(record.lastCleaned, record.catalogData.id, catalog)));
 }
